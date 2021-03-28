90eae6820ed2441df1c6ce6bb0d5fc347ad98c1b
miko0404 <koala20m@icloud.com>
2021-03-28 14:54:06 +0900

    商品詳細機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create,]
+  before_action :set_item, only:[:show]
+  
+
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -16,9 +19,20 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
+  def show
+    
+  end
   
   private
   def items_params
     params.require(:item).permit(:image, :item_name, :item_explanation, :category_id, :item_status_id, :postage_payer_id, :address_id, :days_shipping_id, :exhibition_price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+
+  
 end
