74ddd8286bed4c2e9b5549002d88f98d0e4b5088
miko0404 <koala20m@icloud.com>
2021-04-04 15:44:12 +0900

    購入機能修正6

--- db/migrate/20210330060138_create_buy_addresses.rb
+++ db/migrate/20210330060138_create_buy_addresses.rb
@@ -5,7 +5,7 @@ class CreateBuyAddresses < ActiveRecord::Migration[6.0]
       t.integer :address_id,    null: false
       t.string :city,           null: false
       t.string :city_number,    null: false
-      t.string :building_name,  null: false
+      t.string :building_name
       t.string :phone_number,   null: false
       t.references :user_buy_address, null: false, foreign_key: true
 
